9eca2bab15ef0eff30a058ce4d07a406791dc681
xkaixiong <xkaixiong@qq.com>
2020-11-18 15:29:14 +0800

    deploy

---
+++ precache-manifest.fa3d467b84b3cda2b599b48e3f9eac9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46b0fe91b74ce64ad169",
+    "url": "/money-test-website/css/app.dc9cb559.css"
+  },
+  {
+    "revision": "6c15efb6af16a3311945ac27ebd0348b",
+    "url": "/money-test-website/index.html"
+  },
+  {
+    "revision": "46b0fe91b74ce64ad169",
+    "url": "/money-test-website/js/app.72fc7eae.js"
+  },
+  {
+    "revision": "a677e8ef6d309dc71ac6",
+    "url": "/money-test-website/js/chunk-vendors.8a068929.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/money-test-website/manifest.json"
+  },
+  {
+    "revision": "99379666ae1f8b46febe05a6b076176b",
+    "url": "/money-test-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-test-website/robots.txt"
+  }
+]);
